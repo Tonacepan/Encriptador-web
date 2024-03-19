552ec482ed943a1f87e0a30717f97427279a10f0
Tonatiuh <tylhuicatzip1700@alumno.ipn.mx>
2024-03-18 21:17:01 -0600

    -h

--- funcion/encript.js
+++ funcion/encript.js
@@ -1,45 +1,61 @@
-//Función que encripta un texto con el cifrado César
+//variables principales 
+let texto = "";
+let desplazamiento = 0;
+let resultado = "";
+//Función para asignar el texto de salida
+function asignartexto(elemento, texto) {
+    let titulo = document.querySelector(elemento);
+    titulo.textContent = texto;
+    return;
+}
+//Función para encriptar el texto
 function encriptarcesar(texto, desplazamiento) {
     let resultado = "";
-
     for (let i = 0; i < texto.length; i++) {
-        let caracter = texto[i];
-
-        if (caracter.match(/[a-z]/i)) {
-            let codigo = texto.charCodeAt(i);
-
-            if (codigo >= 65 && codigo <= 90) {
-                caracter = String.fromCharCode(((codigo - 65 + desplazamiento) % 26) + 65);
-            } else if (codigo >= 97 && codigo <= 122) {
-                caracter = String.fromCharCode(((codigo - 97 + desplazamiento) % 26) + 97);
-            }
+        let charCode = texto.charCodeAt(i);
+        if (charCode >= 65 && charCode <= 90) {
+            resultado += String.fromCharCode((charCode - 65 + desplazamiento) % 26 + 65);
+        } else if (charCode >= 97 && charCode <= 122) {
+            resultado += String.fromCharCode((charCode - 97 + desplazamiento) % 26 + 97);
+        } else {
+            resultado += texto.charAt(i);
         }
-
-        resultado += caracter;
     }
-
     return resultado;
 }
 
-// Función que desencripta un texto en el cifrado César
+//Función para desencriptar el texto
 function desencriptarcesar(texto, desplazamiento) {
     let resultado = "";
-
     for (let i = 0; i < texto.length; i++) {
-        let caracter = texto[i];
-
-        if (caracter.match(/[a-z]/i)) {
-            let codigo = texto.charCodeAt(i);
-
-            if (codigo >= 65 && codigo <= 90) {
-                caracter = String.fromCharCode(((codigo - 65 - desplazamiento + 26) % 26) + 65);
-            } else if (codigo >= 97 && codigo <= 122) {
-                caracter = String.fromCharCode(((codigo - 97 - desplazamiento + 26) % 26) + 97);
-            }
+        let charCode = texto.charCodeAt(i);
+        if (charCode >= 65 && charCode <= 90) {
+            resultado += String.fromCharCode((charCode - 65 - desplazamiento + 26) % 26 + 65);
+        } else if (charCode >= 97 && charCode <= 122) {
+            resultado += String.fromCharCode((charCode - 97 - desplazamiento + 26) % 26 + 97);
+        } else {
+            resultado += texto.charAt(i);
         }
-
-        resultado += caracter;
     }
-
     return resultado;
+}
+
+function btnencrypt() {
+    texto = document.getElementById('entrada').value;
+    desplazamiento = parseInt(document.getElementById('clave').value);
+    resultado = encriptarcesar(texto, desplazamiento);
+    document.getElementById('resultado').removeAttribute('onlyread', 'false');
+    console.log(resultado);
+    document.getElementById('resultado').textContent = resultado;
+    document.getElementById('resultado').setAttribute('onlyread', 'true');
+}
+
+function btndecrypt() {
+    texto = document.getElementById('entrada').value;
+    desplazamiento = parseInt(document.getElementById('clave').value);
+    resultado = desencriptarcesar(texto, desplazamiento);
+    document.getElementById('resultado').removeAttribute('onlyread', 'false');
+    console.log(resultado);
+    document.getElementById('resultado').textContent = resultado;
+    document.getElementById('resultado').setAttribute('onlyread', 'true');
 }
